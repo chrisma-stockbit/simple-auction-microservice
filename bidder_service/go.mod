c23b91590c5ecd3832878b235052e9cc273adfc1
Chrisma <chrisma@stockbit.com>
2020-03-17 20:51:33 +0700

    set up simple google grpc server that access the gokit grpc server

---
+++ bidder_service/go.mod
@@ -0,0 +1,13 @@
+module github.com/chrisma-stockbit/simple-auction-microservice/bidder-service
+
+go 1.14
+
+require (
+	github.com/go-kit/kit v0.10.0
+	github.com/golang/protobuf v1.3.5
+	github.com/jinzhu/copier v0.0.0-20190924061706-b57f9002281a
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/sys v0.0.0-20200316230553-a7d97aace0b0 // indirect
+	google.golang.org/genproto v0.0.0-20200316142031-303a05041dad // indirect
+	google.golang.org/grpc v1.28.0
+)
---
+++ bidder_service/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	gokit "github.com/chrisma-stockbit/simple-auction-microservice/bidder-service/infra/gokit"
+	pb "github.com/chrisma-stockbit/simple-auction-microservice/bidder-service/infra/grpc"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", 10000))
+	if err != nil {
+		log.Fatalf("failed to run grpc server: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterBidderServiceServer(grpcServer, gokit.NewGrpcServer())
+	log.Println("serving grpc server..")
+	grpcServer.Serve(ln)
+}
