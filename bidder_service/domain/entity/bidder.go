e34762f53efb2423ea3a76e7ad044de4b19d0412
Chrisma <chrisma@stockbit.com>
2020-03-18 17:03:04 +0700

    refactor: complying with gorm model

--- bidder_service/domain/entity/bidder.go
+++ bidder_service/domain/entity/bidder.go
@@ -1,10 +1,30 @@
 package entity
 
+import (
+	"database/sql"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	uuid "github.com/satori/go.uuid"
+)
+
 type Bidder struct {
-	Guid string
-	FullName string
+	ID            uint
+	CreatedAt     time.Time
+	Guid          string
+	FullName      string
 	AccountNumber string
-	BankName string
-	PhoneNumber string
-	Balance int
+	BankName      string
+	PhoneNumber   string
+	Balance       sql.NullInt64
+	IsActive      bool
+}
+
+func (b *Bidder) BeforeCreate(scope *gorm.Scope) error {
+	uid := uuid.NewV4().String()
+	return scope.SetColumn("Guid", uid)
+}
+
+func (b Bidder) TableName() string {
+	return "bidder"
 }
